df219629cfa046e8b284b7e84f6f5b33633f78c2
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-07-07 06:57:18 +0700

    change layout.app

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Fluid jumbotron</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,34 +21,47 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <!-- #Region Navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+            <a class="navbar-brand" href="{{ route('home') }}">
+                 <span>ProfilManager</span>   
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+                <div class="collapse navbar-collapse" id="navbarSupportedContent"> 
+                    <!-- #Region Left Slide Navbar -->
+                        <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a href="{{ route('home') }}" class="nav-link p-4 active">Home</a>
                             </li>
-                            @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a href="#" class="nav-link p-4 ">Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link p-4 ">Article</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link p-4 ">Gallery</a>
+                            </li>
+                        </ul>
+                    <!-- #Endregion Left Slide Navbar -->
+
+                    <!-- #Region Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- #Region Authentication Links -->
+                            @guest
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link btn btn-warning" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                            @endif
-                        @else
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link btn btn-success ml-3" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -67,14 +80,75 @@
                                 </div>
                             </li>
                         @endguest
-                    </ul>
+                        </ul>
+                    <!-- #Endregion Right Side Of Navbar -->
                 </div>
             </div>
         </nav>
+    <!-- #Endregion Navbar -->
 
+     <!-- #Region Content -->
         <main class="py-4">
             @yield('content')
         </main>
+     <!-- #Region Content -->
+    
+
+    <!-- #Region Footer -->
+        <footer class="text-white bg-primary py-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 text-center text-md-left">
+                    <a href="index.html">
+                        <img src="{{ asset('img/ilkoom_logo.png') }}" style="height: 60px;">
+                    </a>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aperiam cumque, esse modi maxime veniam nulla delectus dolorem
+                    </p>
+                    </div>
+               
+                <div class="col-md-3 text-left">
+                    <h2>Community</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white">Activity</a></li>
+                        <li><a href="#" class="text-white">Member</a></li>
+                        <li><a href="#" class="text-white">Groub</a></li>
+                        <li><a href="#" class="text-white">Groub</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-3 text-left">
+                    <h2>Our Service</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white">Our Vision</a></li>
+                        <li><a href="#" class="text-white">Our Mission</a></li>
+                        <li><a href="#" class="text-white">Privacy Police</a></li>
+                        <li><a href="#" class="text-white">Terms & Condition</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-3 text-left">
+                    <h2>Our Service</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white">prihtapambudi@gmail.com</a></li>
+                        <li><a href="#" class="text-white">082382611951</a></li>
+                        <li><a href="#" class="text-white">www.dwiprihta.github.io</a></li>
+                        <li><a href="#" class="text-white">Karanganyar, Indonesia</a></li>
+                    </ul>
+                </div>
+                </div><hr style="border-color:#fff;">
+                <div class="row mt-3 mt-md-0">
+                    <div class="col-md-3 mr-md-auto text-center text-md-left">
+                        <p>Create dwi prihtapambudi</p>
+                    </div>
+                    <div class="col-md-3 text-right text-md-right">
+                        <p>2020</p>
+                    </div>
+                </div>
+            </div>
+        </footer>   
+    <!-- #Endregion Footer -->
+
     </div>
 </body>
 </html>

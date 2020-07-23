ed713b71f092fba2cf21ff122a63a00213697a4b
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-07-23 20:51:00 +0700

    try to chane dhasboaard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<!-- #region alert -->
+    @if(session()->has('pesan'))
+        <!-- alert update -->
+        @if(session()->has('pesan')=='update')
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            Data <b>{{session()->get('nama')}}</b> berhasil diubah
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @endif
 
-                    You  are logged in!
-                </div>
+        <!-- alert delete -->
+        @if(session()->has('pesan')=='delete')
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            Data <b>{{session()->get('nama')}}</b> berhasil dihapus
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @endif
+    @endif
+<!-- #endregion alert -->
+
+<!-- #region looping for show all user -->
+    <div class="card-columns">
+        @forelese ($users as $user)
+        <div class="card">
+            <!-- background profil -->
+            <img class="card-img-top" src="{{ asset('storage/uploads/'.$user->background_profil)}}" alt="gambar profil user">
+            <!-- profile picture -->
+            <div class="card-body">
+                <img src="{{ asset('storage/uploads/'.$user->gambar_profil)}}" alt="fot profil user" class="roundes-circle img-thumbnail">
             </div>
+            <!-- show name user -->
+            <h5 class="card-tittle">{{$user->nama}}</h5>
+            <!-- show bio user -->
+            <p class="card-text">
+                {{@user->bio_profil}}
+            </p>
+            <ul class="fa-ul text-left">
+                <li class="mb-2">
+                    <span class="fa-li"><i class="far fa-lock"></i></span>
+                    Join in{{ date('F Y', strtotime($user->created_at)) }}
+                </li>
+
+                <li class="mb-2">
+                    <span class="fa-li"><i class="far fa-briefcase"></i></span>
+                    {{($user->pekerjaan) }}
+                </li>
+
+                <li class="mb-2">
+                    <span class="fa-li"><i class="far fa-home"></i></span>
+                    {{ ($user->tempat_lahir) }}
+                </li>
+
+                <li class="mb-2">
+                    <span class="fa-li"><i class="far fa-calender"></i></span>
+                    {{ ($user->tanggal_lahir) }}
+                </li>
+
+                <li class="mb-2">
+                    <span class="fa-li"><i class="far fa-brithday-cake"></i></span>
+                    {{ date_diff(date_create($user->tanggal_lahir),date_create('now'))->y }} tahun
+                </li>
+
+                <li class="mb-2">
+                    <span class="fa-li"><i class="far fa-calender"></i></span>
+                    {{ ($user->tanggal_lahir) }}
+                </li>
+            </ul>
+
+
         </div>
     </div>
-</div>
+<!-- #endregion looping for show all user -->
+
+
 @endsection

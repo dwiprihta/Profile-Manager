ee60713ddbeab5b8733f89275e683c70c8017a0f
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-07-25 21:36:07 +0700

    update register and user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
-    //
+    //for edit user
+    public function edit(User $user){
+        $time=strtotime($user->tanggal_lahhir);
+        $user['tgl']=date('d',$time);
+        $user['bln']=date('m',$time);
+        $user['thn']=date('y',$time);
+
+        return view('user.edit',compact('user'));
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/users/{user}/edit','UserController@edit');
+Route::patch('/users/{users}','Usersontroller@update');
+Route::delete('/users/{user}','UserController@destroy');
+
+

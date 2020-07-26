6117519b10f07726a94cc2f2d052c36165390c81
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-07-26 09:37:31 +0700

    add folder user to contain user view'

---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section ('content')
+    <!-- #region Header image -->
+        <header id="update-header" class="header-image text-white d-none d-md-block">
+            <div class="header-overlay">
+                <div class="container">
+                <div class="row">
+                    <div class="col">
+                    <h1 class="display-1">Join our Community</h1>
+                    <p>Bergabunglah dengan salah satu blog komunitas terbaik
+                        di Indonesia</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </header>
+    <!-- #endregion Header image -->
+
+    <!-- #region form edit -->
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-lg-8">
+                    <h1>Edit Data</h1><hr>
+                    <form method="POST" action="{{url('/users/'.$user->id)}}" enctype="multipart/form-data">
+                    @method('PATCH')
+                    @include('layouts.form',['tombol'=>'Update'])
+                    </form>
+                </div>
+            </div>
+        </div>
+    <!-- #endregion form edit -->
+@endsection
